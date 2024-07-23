b51bb503c2a96d6a0134a97cdd1b7fc7eb619d1c
JK <viletert@gmail.com>
2024-07-23 12:07:21 +0300

    Main section small updates

--- src/components/ButtonAccent.ts
+++ src/components/ButtonAccent.ts
@@ -2,21 +2,22 @@ import styled from "styled-components";
 import {theme} from "../styles/Theme";
 
 export const ButtonAccent = styled.button`
-  font-family: Fira Code,sans-serif;
-  color: ${theme.colors.accentSecondary};
-  padding: 8px 16px;
-  font-size: 16px;
-  font-weight: 500;
-  white-space: nowrap;
-  //text-transform: uppercase;
-  border: 1px solid ${theme.colors.accent};
-  transition: ${theme.animations.transition};
-  
-  :hover {
-    background: rgba(199, 120, 221, 0.20);;
-  }
-  
-  @media ${theme.media.tablet} {
     width: 100%;
-  }
+    font-family: Fira Code, sans-serif;
+    color: ${theme.colors.accentSecondary};
+    padding: 8px 16px;
+    font-size: 16px;
+    font-weight: 500;
+    white-space: nowrap;
+    text-transform: uppercase;
+    border: 1px solid ${theme.colors.accent};
+    transition: ${theme.animations.transition};
+
+    :hover {
+        background: rgba(199, 120, 221, 0.20);;
+    }
+
+    @media ${theme.media.tablet} {
+        width: 100%;
+    }
 `
